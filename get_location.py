697c2cb778fb1b0c1568aeaf43fc923d9e4a66f6
Dorota Karkut <dorakarkut1@wp.pl>
2021-05-14 19:12:34 +0200

    added documentation to get_location

--- get_location.py
+++ get_location.py
@@ -1,16 +1,53 @@
+"""Get location
+
+This script based on IP address gets and returns location of user.
+
+This script requires that `socket and simple_geoip` be installed within the Python
+environment you are running this script in. It also requires module get_api_number 
+from Music_Weather repository.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * get_IP - returns IP address of user
+    * get_location - calls get_IP and get_api functions and returns location of user
+"""
 
 import socket
 from simple_geoip import GeoIP
 from get_api_number import get_api
 
-def get_IP():
+def get_IP() -> int:
+	"""Gets and returns IP address of user
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    int
+        Int which contains IP address
+    """
+
 	host = socket.gethostname()
 	port=0
 	result = socket.getaddrinfo(host, port, socket.AF_INET6)
 	return result[1][4][0]
 	
 
-def get_location():
+def get_location() -> str:
+	"""Calls get_IP and get_api functions and returns location of user
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    str
+        String which contains location
+    """
 	IP = get_IP()
 	api = get_api("location")
 	geoip = GeoIP(api)
@@ -20,7 +57,7 @@ def get_location():
 	
 
 if __name__ == '__main__':
-	
-	print(get_location())
-
+	print(help(get_location))
+	get_location()
+   
 
