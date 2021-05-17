87e68885d43320dcbcc3882d4af521746ffb23f2
Dorota Karkut <dorakarkut1@wp.pl>
2021-05-17 16:52:55 +0200

    CurrentWeather code and documentation update (types of variables)

--- CurrentWeather.py
+++ CurrentWeather.py
@@ -1,18 +1,62 @@
+"""Current Weather
 
-import requests, json, pytemperature
+This script based on location gets and returns current weather: pressure, humidity, weather description, sunrise and sunset time.
+
+This script requires that 'requests', 'json', 'pytemperature', 'time', 'typing' be installed within the Python
+environment you are running this script in. It also requires module get_api_number 
+from Music_Weather repository.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * timestamp2time - based on timestamp return an hour
+	* current_weather - based on location returns current_pressure, current_humidity, weather_description, weather_description_id,sunrise,sunset
+"""
+
+import requests, json, pytemperature, typing
 import numpy as np
 from get_api_number import get_api
 import time
 
 
 def timestamp2time(timestamp):
+	"""
+	Based on timestamp return an hour
+
+    Parameters
+    ----------
+    timestamp : int
+		timestamp
+
+    Returns
+    -------
+    int 
+        Int which contains an hour 
+    """
 	hour = time.strftime("%D %H:%M", time.localtime(int(timestamp)))
 	return hour
 
-def current_weather(city_name,api_key):	
-	
+def current_weather(city_name:str) -> tuple:	
+	"""
+	Based on location returns current_pressure, current_humidity, weather_description, 
+	weather_description_id,sunrise,sunset time
+    
+	Parameters
+    ----------
+    city_name : str
+		String which contains name of the city
+	api_key : object
+		Object which contains API key necessary to connect to the server
+
+    Returns
+    -------
+    tuple
+		Set of information about weather: current_temperature: int, current_pressure: int, current_humidity: int, 
+		weather_description: str, weather_description_id,sunrise,sunset time
+    """
+
 	base_url = "http://api.openweathermap.org/data/2.5/weather?"
-	
+	api_key = get_api("weather")
 	# complete url address
 	complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 	response = requests.get(complete_url)
@@ -23,13 +67,15 @@ def current_weather(city_name,api_key):
 		#unpacking dictionary
 		y = data["main"]
 
-		current_temperature = np.round(pytemperature.k2c(y["temp"]))
-		current_pressure = y["pressure"]
-		current_humidity = y["humidity"]
-		weather_description_id = data["weather"][0]["id"]
-		weather_description = data["weather"][0]["description"]
-		sunrise = timestamp2time(data["sys"]["sunrise"]).split()[1]
-		sunset = timestamp2time(data["sys"]["sunset"]).split()[1]
+		current_temperature = int(np.round(pytemperature.k2c(y["temp"])))
+		current_pressure = int(y["pressure"])
+		current_humidity = int(y["humidity"])
+		weather_description_id = int(data["weather"][0]["id"])
+		weather_description = str(data["weather"][0]["description"])
+		sunrise = timestamp2time(data["sys"]["sunrise"]).split()[1].split(":")
+		sunrise = [ int(x) for x in sunrise]
+		sunset = timestamp2time(data["sys"]["sunset"]).split()[1].split(":")
+		sunset = [ int(x) for x in sunset]
 
 		return (current_temperature, current_pressure, current_humidity, weather_description, weather_description_id,sunrise,sunset)
 	else:
@@ -39,5 +85,5 @@ def current_weather(city_name,api_key):
 
 
 if __name__ == '__main__':
-	api = get_api("weather")
-	print(current_weather("Gdańsk", api))
+	
+	print(current_weather("Gdańsk"))
