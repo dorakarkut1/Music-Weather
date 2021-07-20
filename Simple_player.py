6b221e7cf7b5767a060931097a45bfe2d66295fb
Dorota Karkut <dorakarkut1@wp.pl>
2021-07-20 17:28:01 +0200

    Add buttons

--- Simple_player.py
+++ Simple_player.py
@@ -4,24 +4,32 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.video import Video
 from decision import decision_maker 
+from kivy.core.window import Window
 
 # An App running a video file
 
-class MyVideoApp(App):
-
-    name = decision_maker()
-    def build(self):
-        f = GridLayout(rows=2)
-        b = Button(text="Play")
-        video = Video(source=self.name)
+class MyGrid(GridLayout):
+    def __init__(self,name,**kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.rows=2
+        self.inside = GridLayout(cols=2, row_force_default=True, row_default_height=int(Window.height)/8.9)
+        play = Button(text="PLAY", background_color=[0,0,0,1],size_hint_y=None, height=int(Window.height)/8.9)
+        pause = Button(text="PAUSE", background_color=[0,0,0,1],size_hint_y=None, height=int(Window.height)/8.9)
+        self.inside.add_widget(play)
+        self.inside.add_widget(pause)
+        video = Video(source=name)
         video.state='play'
         video.options = {'eos': 'loop'}
         video.allow_stretch=True
+        self.add_widget(video)
+        self.add_widget(self.inside)
+        
 
-        f.add_widget(video)
-        f.add_widget(b)
+class MyVideoApp(App):
 
-        return f
+    def build(self):
+        name = decision_maker()
+        return MyGrid(name)
     
 
 
