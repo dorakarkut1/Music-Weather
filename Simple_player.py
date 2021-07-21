40ace06802a433acbd850eef30f4174c0a2a6f47
Dorota Karkut <dorakarkut1@wp.pl>
2021-07-21 15:48:34 +0200

    Add file in kivy language

--- Simple_player.py
+++ Simple_player.py
@@ -1,35 +1,23 @@
 import kivy
+from decision import decision_maker
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.video import Video
+from kivy.uix.widget import Widget
 from decision import decision_maker 
 from kivy.core.window import Window
 
 # An App running a video file
 
-class MyGrid(GridLayout):
-    def __init__(self,name,**kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.rows=2
-        self.inside = GridLayout(cols=2, row_force_default=True, row_default_height=int(Window.height)/8.9)
-        play = Button(text="PLAY", background_color=[0,0,0,1],size_hint_y=None, height=int(Window.height)/8.9)
-        pause = Button(text="PAUSE", background_color=[0,0,0,1],size_hint_y=None, height=int(Window.height)/8.9)
-        self.inside.add_widget(play)
-        self.inside.add_widget(pause)
-        video = Video(source=name)
-        video.state='play'
-        video.options = {'eos': 'loop'}
-        video.allow_stretch=True
-        self.add_widget(video)
-        self.add_widget(self.inside)
+class MyGrid(Widget):
+    pass
         
 
-class MyVideoApp(App):
+class MyApp(App):
 
     def build(self):
-        name = decision_maker()
-        return MyGrid(name)
+        return MyGrid()
     
 
 
@@ -37,4 +25,4 @@ class MyVideoApp(App):
 
 if __name__ == '__main__':
     
-    MyVideoApp().run()
+    MyApp().run()
